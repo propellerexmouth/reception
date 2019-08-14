2efa2d8f3d42d255c2c39310ae8779a051b48879
Nathan Budd <buddn07@gmail.com>
2019-08-14 23:39:01 +0100

    Add grid

--- src/App.js
+++ src/App.js
@@ -1,65 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
-import { Layout, List } from 'antd';
+import { Layout, List, Card } from 'antd';
+import axios from "axios";
 
 import './App.css';
 
-const { Sider, Content } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
-function App() {
-  const [logs, setLogs] = useState([]);
+const App = () => {
+  const [resp, setRespData] = useState({ logs: null, tags: null });
 
-  const ws = new WebSocket(`ws://${process.env.REACT_APP_ENDPOINT}`);
-  ws.onopen = () => {
-    // alert('test');
+  const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
   };
 
   useEffect(() => {
-    if (logs.length < 1) {
-      fetch(`http://${process.env.REACT_APP_ENDPOINT}/logs`)
-        .then(res => res.json())
-        .then(json => {
-          setLogs(json.data);
-        });
-    }
-  }, [logs]);
+    const fetch1 = axios.get(`http://${process.env.REACT_APP_ENDPOINT}/logs`);
+    const fetch2 = axios.get(`http://${process.env.REACT_APP_ENDPOINT}/tags`);
+    Promise.all([fetch1, fetch2]).then(([res1, res2]) => {
+      setRespData({ logs: res1.data.data, tags: res2.data.data });
+    });
+  }, []);
 
-  console.log(logs);
+  const logList = (resp.logs) ? 
+  <Sider
+    theme="light"
+    width={300}
+    style={{
+      overflow: 'auto',
+      height: '100vh',
+      position: 'fixed',
+      left: 0,
+    }}
+  >
+    <List
+      itemLayout="horizontal"
+      dataSource={resp.logs.map(log => ({
+        name: `${log.first_name} ${log.last_name}`,
+        direction: log.direction,
+        time: moment(log.time).fromNow(),
+      }))}
+      style={{ textAlign: 'left' }}
+      renderItem={item => (
+        <List.Item style={{ padding: 16 }}>
+          <List.Item.Meta title={item.name} description={(item.direction === 1 ? 'In: ' : 'Out: ') + item.time} />
+        </List.Item>
+      )}
+    />
+  </Sider>
+  : <p>Log is empty</p>;
+
+  const tagList = (resp.tags) ?
+  <div>
+    <Card title="Members">
+    {resp.tags.map(tag => (
+      <Card.Grid style={{width: '25%', textAlign: 'center'}}>{tag.first_name} {tag.last_name}</Card.Grid>
+    ))}
+    </Card>
+  </div>
+    : <p>Tag is empty</p>;
+  ;
 
   return (
-    <div className="App">
-      <Sider
-        theme="light"
-        width={300}
-        style={{
-          borderRight: '1px solid #ccc',
-          minHeight: '100vh',
-        }}
-      >
-        <List
-          itemLayout="horizontal"
-          dataSource={logs.map(log => ({
-            name: `${log.first_name} ${log.last_name}`,
-            direction: log.direction,
-            time: moment(log.time).fromNow(),
-          }))}
-          style={{ textAlign: 'left' }}
-          renderItem={item => (
-            <List.Item style={{ padding: 16 }}>
-              <List.Item.Meta
-                title={item.name}
-                description={
-                  item.direction === 1 ? 'In' : 'Out' + ' : ' + item.time
-                }
-              />
-            </List.Item>
-          )}
-        />
-      </Sider>
-      <Content>
-        <h2>People</h2>
-      </Content>
-    </div>
+    <main className="app">
+      <Layout>
+        {logList}
+        <Layout style={{ marginLeft: 300 }}>
+          {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
+          <Content style={{ height: '100vh', margin: '24px 16px 0', overflow: 'initial' }}>
+            <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+              {tagList}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    </Layout>,
+    </main>
   );
 }
 
